0bebf8a6914646dd2b4485e8af744c972091077f
klanc0403 <kelly@kellyisasinger.com>
2026-05-15 14:14:36 -0400

    Added Dictionary container

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-     
-        <p>Hello world</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello world</p>
+        </header>
+      </div>
+      <footer>Coded by Kelly Lançon: Github + Netlify.</footer>
     </div>
   );
 }
