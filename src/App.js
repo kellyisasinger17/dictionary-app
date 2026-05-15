dcdd9a4e0196ab1cc4284dfceb0bfe53b0332fe3
klanc0403 <kelly@kellyisasinger.com>
2026-05-15 15:37:46 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <Dictionary />
+      <footer className="App-footer">
+        Coded by Kelly Singer Lançon:{" "}
+        <a href="https://github.com/kellyisasinger17">Github</a> +{" "}
+        <a href="https://interactivedictionaryapp.netlify.app/">Netlify</a>
+      </footer>
     </div>
   );
 }
